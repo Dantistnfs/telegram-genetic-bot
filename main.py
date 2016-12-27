1de59d09dc93ecc439644497aea9ebc191341290
dantistnfs <dantistnfs@gmail.com>
2016-12-27 20:58:55 +0200

    Added help function

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ rt_dict = {'A':'T','T':'A','G':'C','C':'G'}
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
+bot_starting_message = "Hi i'm telegram bot and your personal assistant, more info with \"\\help\" command"
+bot_help_meggase = "Help is still in development."
+
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.sendMessage(chat_id=update.message.chat_id, text=bot_starting_message)
+
+
+def help(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=bot_help_meggase)
+
 
 def ReverseTranscription(bot, update, args):
     seq = ' '.join(args).upper()
@@ -26,13 +34,15 @@ def ReverseTranscription(bot, update, args):
     bot.sendMessage(chat_id=update.message.chat_id, text=text_for_rt)
 
 
-
 start_handler = CommandHandler('start', start)
+help_handler = CommandHandler('help', help)
 RT_handler = CommandHandler('RT', ReverseTranscription, pass_args=True)
 
 
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(help_handler)
 dispatcher.add_handler(RT_handler)
+
 
 updater.start_polling()
 
