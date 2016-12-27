134ccc3dd12bd8ddf25a2a7b8cb055b36a4166cf
dantistnfs <dantistnfs@gmail.com>
2016-12-27 22:06:11 +0200

    /gene_id function written

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
     level=logging.INFO)
 
 bot_starting_message = "Hi i'm telegram bot and your personal assistant, more info with \"/help\" command"
-bot_help_meggase = "Help is still in development. But you can try to use some functions : \"/gene [argument]\" ; \"/RT [argument]\""
+bot_help_meggase = "Help is still in development. But you can try to use some functions : \"/gene_id [argument]\" ; \"/RT [argument]\""
 
 
 def start(bot, update):
@@ -37,21 +37,26 @@ def ReverseTranscription(bot, update, args):
     text_for_rt = "".join([rt_dict[base] for base in reversed(seq)])
     bot.sendMessage(chat_id=update.message.chat_id, text=text_for_rt)
 
-def GeneSearcher(bot, update, args):
-    gene = ' '.join(args).lower()
-    egs = ec.esearch(db='gene',term=gene)
-    bot.sendMessage(chat_id=update.message.chat_id, text=egs)
+def GeneSearcherID(bot, update, args):
+    gene = ' '.join(args)
+    #egr = ec.esearch(db='gene',term=gene)
+    egs = ec.efetch(db='gene', id=gene)
+    #formated_answer = "Found %d results.\n" (% egr.count)
+    #formated_answer += ""
+    eg = egs.entrezgenes[0]
+    answer = ' '.join([eg.hgnc, eg.maploc, eg.description, eg.type, eg.genus_species,'\n'])
+    bot.sendMessage(chat_id=update.message.chat_id, text=answer)
 
 start_handler = CommandHandler('start', start)
 help_handler = CommandHandler('help', help)
 RT_handler = CommandHandler('RT', ReverseTranscription, pass_args=True)
-Gene_search_handler = CommandHandler('gene', GeneSearcher, pass_args=True)
+Gene_search_id_handler = CommandHandler('gene_id', GeneSearcherID, pass_args=True)
 
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(help_handler)
 dispatcher.add_handler(RT_handler)
-dispatcher.add_handler(Gene_search_handler)
+dispatcher.add_handler(Gene_search_id_handler)
 
 updater.start_polling()
 
