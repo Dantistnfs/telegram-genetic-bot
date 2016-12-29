6d0d62dca7ce25b49a72dd72bcb0915c16b89e5b
dantistnfs <dantistnfs@gmail.com>
2016-12-30 01:49:48 +0200

    Tesseract code changes

--- handlers/sequence_ocr/ocr.py
+++ handlers/sequence_ocr/ocr.py
@@ -2,11 +2,11 @@ try:
     import Image
 except ImportError:
     from PIL import Image
-import pytesseract
-
+    
+from . import pytesseract
 import requests
 from io import BytesIO
 
 def sequence_ocr_processing(image_url):
-	
+
 	return pytesseract.image_to_string(Image.open(BytesIO(requests.get(image_url).content)), lang='eng')
