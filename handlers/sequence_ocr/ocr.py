3a1fa91670e7bb6a793fd5447d3fcdf9bd7840b2
dantistnfs <dantistnfs@gmail.com>
2016-12-30 10:13:12 +0200

    One more try with time counting

--- handlers/sequence_ocr/ocr.py
+++ handlers/sequence_ocr/ocr.py
@@ -10,9 +10,10 @@ import time
 
 
 def sequence_ocr_processing(image_url):
-	image_downloaded = Image.open(BytesIO(requests.get(image_url).content))
 	start = time.time()
+	image_downloaded = Image.open(BytesIO(requests.get(image_url).content))
+	download = time.time() - start
 	recognised_string = pytesseract.image_to_string(image_downloaded, lang='eng').replace("\n","").upper()
-	end = time.time() - start
-	message = 'Operation tooked: %f seconds. \n Here your sequence: \n %s' % (end, recognised_string)
+	end = time.time() - start - download
+	message = 'Download time: %f sec. \n Operation tooked: %f sec. \n Here your sequence: \n %s' % (download, end, recognised_string)
 	return message
